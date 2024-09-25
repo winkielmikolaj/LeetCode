8d6b1ad090723c8276ff37615a8c133ecbcfe721
Mikołaj <pzx112240@student.poznan.merito.pl>
2024-09-25 11:40:48 +0200

    #2769

---
+++ LeetCodeSolutions/#2769_Find_the_Maximum_Achievable_Number.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.LeetCodeSolutions
+{
+    internal class _2769_Find_the_Maximum_Achievable_Number
+    {
+        public int TheMaximumAchievableX(int num, int t)
+        {
+
+
+
+
+            return 2 * t + num;
+
+        }
+    }
+}
