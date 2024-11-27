a20e521249dee0eb006a7c4916948d21091827b4
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2024-11-27 21:25:27 +0100

    #3280

---
+++ LeetCodeSolutions/3280_Convert_Date_to_Binary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.LeetCodeSolutions
+{
+    internal class _3280_Convert_Date_to_Binary
+    {
+        public static string ConvertDateToBinary(string date)
+        {
+            string[] spliting = date.Split("-");
+
+            string year = spliting[0];
+            string month = spliting[1];
+            string day = spliting[2];
+
+            int yearInt = Convert.ToInt32(year);
+            int monthInt = Convert.ToInt32(month);
+            int dayInt = Convert.ToInt32(day);
+
+            string binarna = yearInt.ToString("B");
+
+            string binarnb = monthInt.ToString("B");
+
+            string binarnc = dayInt.ToString("B");
+
+            string ans = $"{binarna}-{binarnb}-{binarnc}";
+
+            Console.WriteLine(ans);
+
+            return ans;
+        }
+    }
+}
+
+//Copy to main
+//ConvertDateToBinary("2080-02-29");
--- Program.cs
+++ Program.cs
@@ -13,31 +13,14 @@ namespace LeetCode
             //Hello into my LeetCode solutions
             //You can find the solutions in the LeetCodeSolutions folder
 
-            MaximumWealth([[1, 5], [99, 1], [3, 5]]);
+            
+
 
             Console.ReadLine();
         }
 
-        public static int MaximumWealth(int[][] accounts)
-        {
-            int sum = 0;
 
-            int theBiggest = 0;
 
-            for (int i = 0; i < accounts.Length; i++)
-            {
-                sum = accounts[i].Sum();
-
-                if(sum > theBiggest)
-                {
-                    theBiggest = sum;
-                }
-            }
-
-            Console.WriteLine(theBiggest);
-
-            return theBiggest;
-        }
 
     }
 }
