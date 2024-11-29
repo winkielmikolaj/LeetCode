c997082b910fa3e693e49a1dcd16dcb51e097fb5
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2024-11-29 23:05:46 +0100

    #2235

---
+++ LeetCodeSolutions/#2235_Add_Two_Integers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.LeetCodeSolutions
+{
+    internal class _2235_Add_Two_Integers
+    {
+
+        public static int Sum(int num1, int num2)
+        {
+
+            int sum = num1 + num2;
+
+            Console.WriteLine(sum);
+
+            return num1 + num2;
+        }
+    }
+}
+
+//Copy to main
+//Sum(12, 5); 
--- Program.cs
+++ Program.cs
@@ -13,11 +13,20 @@ namespace LeetCode
             //Hello into my LeetCode solutions
             //You can find the solutions in the LeetCodeSolutions folder
 
-
+            Sum(12, 5);
 
             Console.ReadLine();
         }
 
+        public static int Sum(int num1, int num2)
+        {
+
+            int sum = num1 + num2;
+
+            Console.WriteLine(sum);
+
+            return num1 + num2;
+        }
     }
 }
 
