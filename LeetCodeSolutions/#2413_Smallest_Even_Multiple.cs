9a064d0cb838e6b5b2a1225e0afd083da1066d11
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2024-11-29 23:43:02 +0100

    #2413

---
+++ LeetCodeSolutions/#2413_Smallest_Even_Multiple.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.LeetCodeSolutions
+{
+    internal class _2413_Smallest_Even_Multiple
+    {
+        public static int SmallestEvenMultiple(int n) //2 i n 
+        {
+            int ans = 0;
+
+            if (n % 2 == 0)
+            {
+                ans = n;
+            }
+            else
+            {
+                ans = 2 * n;
+            }
+
+            Console.WriteLine(ans);
+
+            return ans;
+        }
+    }
+}
+
+//Copy to main
+//SmallestEvenMultiple(6);
--- Program.cs
+++ Program.cs
@@ -13,23 +13,11 @@ namespace LeetCode
             //Hello into my LeetCode solutions
             //You can find the solutions in the LeetCodeSolutions folder
 
-            GetSum(12, 5);
+
 
             Console.ReadLine();
         }
 
-        public static int GetSum(int a, int b)
-        {
-            int[] array = { a, b };
-
-            int ans = 0;
-
-            ans = array.Sum();
-
-            Console.WriteLine(ans);
-
-            return ans;
-        }
     }
 }
 
