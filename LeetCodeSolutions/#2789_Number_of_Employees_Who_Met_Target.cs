e9fa05dbd3909e40455daeadb00cebb29fe7707f
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2024-12-04 22:37:43 +0100

    #2798

---
+++ LeetCodeSolutions/#2789_Number_of_Employees_Who_Met_Target.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.LeetCodeSolutions
+{
+    internal class _2789_Number_of_Employees_Who_Met_Target
+    {
+        public static int NumberOfEmployeesWhoMetTarget(int[] hours, int target)
+        {
+            int count = 0;
+
+            for (int i = 0; i < hours.Length; i++)
+            {
+                if (hours[i] >= target)
+                {
+                    count++;
+                }
+            }
+
+
+            return count;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,38 +13,12 @@ namespace LeetCode
             //Hello into my LeetCode solutions
             //You can find the solutions in the LeetCodeSolutions folder
 
-            KidsWithCandies([2, 3, 5, 1, 3], 3);
+
 
             Console.ReadLine();
         }
 
-        public static IList<bool> KidsWithCandies(int[] candies, int extraCandies)
-        {
-            IList<bool> result = new List<bool>();
-            int max = candies.Max();
 
-            for (int i = 0; i < candies.Length; i++)
-            {
-                candies[i] += extraCandies;
-
-                if (candies[i] >= max)
-                {
-                    result.Add(true);
-                    //Console.WriteLine(true);
-                }
-                else
-                {
-                    result.Add(false);
-                }
-            }
-
-            foreach (var resu in result)
-            {
-                Console.WriteLine(resu);
-            }
-
-                return result;
-        }
 
     }
 }
