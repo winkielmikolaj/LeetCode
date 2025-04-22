c71a3fa3c7719ed8a4aae607dcc81882fddc287f
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2025-04-22 21:18:14 +0200

    #242

---
+++ LeetCodeSolutions/#242_Valid_Anagram.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.LeetCodeSolutions;
+
+public class _242_Valid_Anagram
+{
+    public bool IsAnagram(string s, string t)
+    {
+        //yeah, it is boring bruteforce but hashmap approach was too hard
+        char[] sArray = s.ToCharArray();
+        char[] tArray = t.ToCharArray();
+        Array.Sort(sArray);
+        Array.Sort(tArray);
+                
+        if (new string(sArray) == new string(tArray))
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
