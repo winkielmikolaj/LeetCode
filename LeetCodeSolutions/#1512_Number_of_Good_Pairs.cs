a198a4aeb49190324d1f99d493e5c4d8d06e173c
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2025-04-28 12:45:44 +0200

    #1512

---
+++ LeetCodeSolutions/#1512_Number_of_Good_Pairs.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.LeetCodeSolutions;
+
+public class _1512_Number_of_Good_Pairs
+{
+    
+    //#1 solution, there is better one
+    public int NumIdenticalPairs(int[] nums)
+    {
+        int goodPairs = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (nums[i] == nums[j] && i < j)
+                {
+                    goodPairs++;
+                }
+            }
+        }
+
+        Console.WriteLine(goodPairs);
+        return goodPairs;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -14,13 +14,30 @@ namespace LeetCode
             //Hello into my LeetCode solutions
             //You can find the solutions in the LeetCodeSolutions folder
 
-            //var soulution = new Solution();
-            //soulution.
+            var soulution = new Solution();
+            soulution.NumIdenticalPairs([1, 2, 3, 1, 1, 3]);
         }
 
         public class Solution
         {
-            
+            public int NumIdenticalPairs(int[] nums)
+            {
+                int goodPairs = 0;
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    for (int j = 0; j < nums.Length; j++)
+                    {
+                        if (nums[i] == nums[j] && i < j)
+                        {
+                            goodPairs++;
+                        }
+                    }
+                }
+
+                Console.WriteLine(goodPairs);
+                return goodPairs;
+            }
         }
     }
 }
