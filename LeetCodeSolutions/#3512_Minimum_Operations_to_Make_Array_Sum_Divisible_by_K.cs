3040c2e04330d293f3680ea357d5ad496fe8274d
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2025-05-19 20:06:27 +0200

    #3512

---
+++ LeetCodeSolutions/#3512_Minimum_Operations_to_Make_Array_Sum_Divisible_by_K.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.LeetCodeSolutions;
+
+public class _3512_Minimum_Operations_to_Make_Array_Sum_Divisible_by_K
+{
+    public int MinOperations(int[] nums, int k)
+    {
+        int sum = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+        }
+
+        return sum % k;
+    }
+}
